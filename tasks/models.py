b37a7225f4276318d50bbdc924fde4f94f839ecf
Thobani <mjiyakhothobani@gmail.com>
2024-03-24 22:04:52 +0200

    task model

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    status = models.CharField(max_length=20, default='ToDo')
