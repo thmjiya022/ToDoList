9d97cb52de016236e012c0a2293c6726b08b9748
Thobani <mjiyakhothobani@gmail.com>
2024-03-24 23:06:52 +0200

    update status

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('update/<str:pk>', views.update_status, name='update_status')
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
 
 # Create your views here.
 def home(request):
-    return render(request, 'tasks/home.html')
+    tasks = Task.objects.all()
+    form = TaskForm()
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('home')
+    
+    context = {
+        'tasks': tasks,
+        'form': form,
+    }
+
+    return render(request, 'tasks/home.html', context)
+
+
+def update_status(request,pk):
+    task = Task.objects.get(pk=pk)
+
+    if task.status == 'ToDo':
+        task.status = 'InProgress'
+    elif task.status == 'InProgress':
+        task.status = 'Completed'
+    task.save()
+
+    return redirect('home')
